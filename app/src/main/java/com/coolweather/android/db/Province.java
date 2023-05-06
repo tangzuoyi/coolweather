f517ab8a170fddcf2a144a8c57de7e5065de3bc5
tangzuoyi <544384712@qq.com>
2023-05-06 17:22:49 +0800

    add database1

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -8,6 +8,26 @@ public class Province extends LitePalSupport {
     @Column(unique = false,defaultValue = "unknown")
     public int id;
 
+    private String provinceName;
+
+    private int provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
     public int getId() {
         return id;
     }
