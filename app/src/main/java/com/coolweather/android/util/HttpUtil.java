a1ce61a492fb38a1788c05f62c7a4ed660f8b978
tangzuoyi <544384712@qq.com>
2023-05-06 17:45:30 +0800

    202305061745代码

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,9 @@
+package com.coolweather.android.util;
+
+import javax.security.auth.callback.Callback;
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callBack){
+
+    }
+}
